8ae705905c3b8fd19145c8a53ccf1428365774ed
Stephanie Keske <skeske@gmail.com>
2020-12-10 01:19:14 -0800

    implementing router

---
+++ src/Avatars.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Avatars() {
+  return (
+    <>
+        AVATARS
+    </>
+  );
+}
+
+export default Avatars;
---
+++ src/routes.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Avatars from "./Avatars";
+import Context from "./Context";
+import { Switch, Route } from "react-router-dom";
+
+const ROUTES = [
+  {
+    path: "/",
+    key: "APP",
+    component: RenderRoutes,
+    routes: [
+      {
+        path: "/context",
+        key: "APP_PAGE",
+        exact: true,
+        component: Context  
+      },
+      {
+        path: "/avatars",
+        key: "APP_ROOT",
+        exact: true,
+        component: Avatars
+      },
+    ],
+  },
+];
+
+export default ROUTES;
+
+/* *
+ * Render a route with potential sub routes
+ * https://reacttraining.com/react-router/web/example/route-config
+ */
+function RouteWithSubRoutes(route) {
+    return (
+      <Route
+        path={route.path}
+        exact={route.exact}
+        render={props => <route.component {...props} routes={route.routes} />}
+      />
+    );
+  }
+
+/* *
+ * Use this component for any new section of routes (any config object that has a "routes" property
+ */
+export function RenderRoutes({ routes }) {
+    return (
+      <Switch>
+        {routes.map((route, i) => {
+          return <RouteWithSubRoutes key={route.key} {...route} />;
+        })}
+      </Switch>
+    );
+  }
