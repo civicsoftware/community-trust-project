2730765cf7e37ad97e18989f0e78b1362c1d0f0f
Stephanie Keske <skeske@gmail.com>
2020-12-30 15:27:31 -0800

    adding windmill button theming

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import { default as Windmill } from "@windmill/react-ui";
 import ROUTES, { RenderRoutes } from "./routes";
 import Navbar from "./Navbar";
 import "./fonts.css";
+import trustTheme from './trustTheme';
 
 function App() {
   return (
-    <Windmill.Windmill>
+    <Windmill.Windmill theme={trustTheme}>
       <Navbar />
       <RenderRoutes routes={ROUTES} />
     </Windmill.Windmill>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { default as Windmill } from "@windmill/react-ui";
-import Compass from "./Compass";
+// import Compass from "./Compass";
+import FullLogo from "../public/trust_logo_full.svg";
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -13,55 +14,73 @@ function Home() {
 
   return (
     <>
-      {/* Hero Card */}
-      <div className="container mx-auto m-6 px-4 grid grid-cols-1 justify-center">
-        <p className="text-6xl mb-4 mt-20 font-semibold justify-self-center">
-          Community data, in context.
-        </p>
-        <div className="text-xl justify-self-center">
-          This is a very good mission statement that is concise and inspiring.
-          Collaboration to center community.
-        </div>
-        <div className="text-4xl justify-self-center mt-40">
-          <a
-            href="/library"
-            className="whitespace-nowrap text-gray-500 px-4 py-2 hover:text-gray-900"
-          >
-            → Explore the Data Library ←
-          </a>
-          <p className="text-xl justify-self-center">
-            (maybe there is a cool graphic here)
-          </p>
-        </div>
+      <div className="container mx-auto m-6 mt-20 px-4 flex flex-col">
+        <section id="hero" className="">
+          <div className="flex flex-col justify-center text-center">
+            <img src={FullLogo} className={"w-2/5 pb-5 self-center"} />
+            <p className="text-2xl">
+              The Community TRUST Project represents a commitment from local
+              government to assess institutional bias embedded in data models and
+              systems that represent diverse identities and lived experiences of
+              the communities they serve.
+            </p>
+            <p className="text-xl mb-2 mt-12 font-semibold self-center">
+              Bring TRUST to your community.
+            </p>
+            <Windmill.Button
+              onClick={openModal}
+              className="whitespace-nowrap inline-flex self-center w-2/5 px-4 py-2 m-1 border border-transparent rounded-md shadow-sm text-base font-medium text-white"
+            >
+              Click here to get involved
+            </Windmill.Button>
+          </div>
+        </section>
 
-        {/* <Compass /> */}
-
-        <div className="bg-gray-50 mt-40">
-          <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-around">
-            <h2 className="text-3xl tracking-tight text-gray-900 sm:text-4xl">
-              <span className="block">
-                We&apos;re building something important.
-              </span>
-              <span className="block text-indigo-600">Want to be a part?</span>
-            </h2>
-            <div className="mt-8 lex lg:mt-0 lg:flex-shrink-0">
-              <Windmill.Button
-                onClick={openModal}
-                className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 m-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Contribute Your Data
-              </Windmill.Button>
-              <Windmill.Button
-                onClick={openModal}
-                className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 m-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Request a Dataset
-              </Windmill.Button>
+        <section id="blueprint">
+          <div className="bg-gray-50 mt-20">
+            <div className="max-w-7xl mx-auto py-12 px-4">
+              <h2 className="text-2xl text-gray-900">
+                Community Library of Context
+              </h2>
+              <p className="block text-pink">
+                  The Community Library of Context is a resource that reflects
+                  an active anti-oppression practice, and a structured process
+                  of creating metadata context for datasets that can be curated
+                  collaboratively and shared publicly.
+              </p>
+              <div className="mt-8 lex lg:mt-0 lg:flex-shrink-0">
+                Learn more about the CIVIC Blueprint for Context-Aware Systems
+                <Windmill.Button
+                  onClick={openModal}
+                  className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 m-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-pink-dark hover:bg-pink-light"
+                >
+                  Click here
+                </Windmill.Button>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+
+        <section id="library">
+          <div className="text-4xl justify-self-center mt-40">
+            <a
+              href="/library"
+              className="whitespace-nowrap text-gray-500 px-4 py-2 hover:text-gray-900"
+            >
+              → Explore the Data Library ←
+            </a>
+            <p className="text-xl justify-self-center">
+              (maybe there is a cool graphic here)
+            </p>
+          </div>
+        </section>
+
+        <section id="compass"></section>
+
+        <section id="stakeholders"></section>
+
+        <section id="flower"></section>
       </div>
-      {/* End hero card */}
 
       {/* Modals for data submission forms */}
       <Windmill.Modal isOpen={isModalOpen} onClose={closeModal}>
