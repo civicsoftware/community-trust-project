16faa16fcef645a94c6cc97de09e5b9477d0b80f
Stephanie Keske <skeske@gmail.com>
2020-12-30 18:21:13 -0800

    linter fixes

--- src/wip/wip_interactive_Compass.jsx
+++ src/wip/wip_interactive_Compass.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { default as Windmill } from "@windmill/react-ui";
 import React, { useState } from "react";
-import "./Compass.css";
+import "./wip_interactive_Compass.css";
 
 function Compass() {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,68 +35,68 @@ function Compass() {
         </div>
       </div>
 
-    {/* TODO: slice 1-5 click handling doesn't work because of mask overlay? */}
-    {isOpen && (
-    <Windmill.Backdrop onClick={toggleBackdrop} className="z-0">
-    <div className="relative">
-      {cardShown && (
-        <div className="definitionCard">
-          <div onClick={e => toggleCard(e)}> X </div>
-          <div className="content">
-            Definition
-            <p>
-              <a href="/avatars">Click to hear stories</a>
-            </p>
+      {/* TODO: slice 1-5 click handling doesn't work because of mask overlay? */}
+      {isOpen && (
+        <Windmill.Backdrop onClick={toggleBackdrop} className="z-0">
+          <div className="relative">
+            {cardShown && (
+              <div className="definitionCard">
+                <div onClick={e => toggleCard(e)}> X </div>
+                <div className="content">
+                  Definition
+                  <p>
+                    <a href="/avatars">Click to hear stories</a>
+                  </p>
+                </div>
+              </div>
+            )}
+            <div className="compassContainer">
+              <div className="outerCircle" />
+              <div className="innerCircle">
+                <div className="content">The Compass of Oppression</div>
+              </div>
+              <div className="pieContainer">
+                <div id="pieSlice1" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice1")} />
+                </div>
+                <div id="pieSlice2" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice2")} />
+                </div>
+                <div id="pieSlice3" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice3")} />
+                </div>
+                <div id="pieSlice4" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice4")} />
+                </div>
+                <div id="pieSlice5" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice5")} />
+                </div>
+                <div id="pieSlice6" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice6")} />
+                </div>
+                <div id="pieSlice7" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice7")} />
+                </div>
+                <div id="pieSlice8" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice8")} />
+                </div>
+                <div id="pieSlice9" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice9")} />
+                </div>
+                <div id="pieSlice10" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice10")} />
+                </div>
+                <div id="pieSlice11" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice11")} />
+                </div>
+                <div id="pieSlice12" className="hold">
+                  <div className="pie" onClick={e => printPie(e, "slice12")} />
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+        </Windmill.Backdrop>
       )}
-      <div className="compassContainer">
-        <div className="outerCircle" />
-        <div className="innerCircle">
-          <div className="content">The Compass of Oppression</div>
-        </div>
-        <div className="pieContainer">
-          <div id="pieSlice1" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice1")} />
-          </div>
-          <div id="pieSlice2" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice2")} />
-          </div>
-          <div id="pieSlice3" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice3")} />
-          </div>
-          <div id="pieSlice4" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice4")} />
-          </div>
-          <div id="pieSlice5" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice5")} />
-          </div>
-          <div id="pieSlice6" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice6")} />
-          </div>
-          <div id="pieSlice7" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice7")} />
-          </div>
-          <div id="pieSlice8" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice8")} />
-          </div>
-          <div id="pieSlice9" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice9")} />
-          </div>
-          <div id="pieSlice10" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice10")} />
-          </div>
-          <div id="pieSlice11" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice11")} />
-          </div>
-          <div id="pieSlice12" className="hold">
-            <div className="pie" onClick={e => printPie(e, "slice12")} />
-          </div>
-        </div>
-      </div>
-    </div>
-    //   </Windmill.Backdrop>
-    )}
     </>
   );
 }
