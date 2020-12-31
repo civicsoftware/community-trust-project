19c7b3ede76918632484071d32ad3b0444a10657
Stephanie Keske <skeske@gmail.com>
2020-12-31 02:12:48 -0800

    homepage and navbar style tweaks

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { default as Windmill } from "@windmill/react-ui";
 import FullLogo from "../public/trust_logo_full.svg";
+import CivicLogo from "../public/CIVIC_LOGO.svg";
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,30 +17,38 @@ function Home() {
     <>
       <section
         id="hero"
-        className="bg-gray-50 border-gray-300 border-2 top-0 pt-20 pb-16"
+        className="bg-gray-200 border-gray-300 border-2 top-0 pt-20 pb-16"
       >
-        <div className="container mx-auto px-60 grid grid-cols-5 gap-x-12 justify-items-stretch items-start">
-          <img
-            src={FullLogo}
-            className="col-span-2 pb-5 justify-self-end pt-2"
-            alt="Trust Project Logo"
-          />
-          <div className="col-span-3">
-            <p className="text-2xl justify-self-start">
+        <div className="container mx-auto px-72 grid grid-cols-4 gap-x-12 justify-items-stretch items-start">
+          <div className="col-span-2">
+            <img
+              src={FullLogo}
+              className="pb-5 justify-self-end pt-2"
+              alt="Trust Project Logo"
+            />
+            <p className="text-sm -mt-9">
+              <a
+                href="http://www.civicplatform.org"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Built by Civic Software Foundation
+              </a>
+            </p>
+          </div>
+          <div className="col-span-2">
+            <p className="text-xl justify-self-start">
               The Community TRUST Project represents a commitment from local
               government to assess institutional bias embedded in data models
               and systems that represent diverse identities and lived
               experiences of the communities they serve.
             </p>
-            <div className="justify-self-center self-start pt-8 pb-2 mb-4 flex flex-row">
-              <p className="pt-1 text-xl font-semibold">
-                Bring TRUST to your community:
-              </p>
+            <div className="justify-self-center self-start pt-4 pb-2 mb-4 flex flex-row">
               <Windmill.Button
                 onClick={openModal}
-                className="whitespace-nowrap ml-4 px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white"
+                className="whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white"
               >
-                Click here to get involved
+                Bring TRUST to your community
               </Windmill.Button>
             </div>
           </div>
@@ -50,7 +59,7 @@ function Home() {
         <section id="blueprint">
           <div className="m-12 max-w-7xl p-12">
             <div className="flex items-center text-gray-900 mb-4">
-              <svg
+              {/* <svg
                 className="h-6 w-6 mr-3"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -64,7 +73,12 @@ function Home() {
                   strokeWidth="2"
                   d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                 />
-              </svg>
+              </svg> */}
+              <img
+                src={CivicLogo}
+                className="w-7 pt-1 mr-3"
+                alt="Trust Project Logo"
+              />
               <h2 className="text-2xl font-semibold">
                 Community Library of Context
               </h2>
@@ -76,12 +90,9 @@ function Home() {
               collaboratively and shared publicly.
             </p>
             <div className="mt-8 font-semibold flex flex-row">
-              <p className="pt-2">
-                Learn more about the CIVIC Blueprint for Context-Aware Systems:
-              </p>
               <Link to="/blueprint">
                 <Windmill.Button className="w-65 ml-3 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white">
-                  Click here
+                  Learn more about the CIVIC Blueprint for Context-Aware Systems
                 </Windmill.Button>
               </Link>
             </div>
@@ -104,7 +115,7 @@ function Home() {
 
         <section id="compass">
           <div className="m-12 mt-24 max-w-7xl px-12">
-            <div className="flex items-center text-gray-900 mb-4">
+            <div className="flex items-center text-blue mb-4">
               <svg
                 className="h-6 w-6 mr-3"
                 xmlns="http://www.w3.org/2000/svg"
@@ -120,7 +131,7 @@ function Home() {
                   d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4"
                 />
               </svg>
-              <h2 className="text-2xl font-semibold">
+              <h2 className="text-2xl font-semibold text-gray-900 ">
                 Everything is Intersectional
               </h2>
             </div>
@@ -130,10 +141,9 @@ function Home() {
               understanding how to do work in our current environment.
             </p>
             <div className="mt-8 font-semibold flex flex-row">
-              <p className="pt-2">Navigate the Compass of Oppression:</p>
               <Link to="/compass">
                 <Windmill.Button className="w-65 ml-3 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white">
-                  Click here
+                  Navigate the Compass of Oppression
                 </Windmill.Button>
               </Link>
             </div>
@@ -142,7 +152,7 @@ function Home() {
 
         <section id="stakeholders">
           <div className="m-12 mt-24 max-w-7xl px-12">
-            <div className="flex items-center text-gray-900 mb-4">
+            <div className="flex items-center text-yellow-dark mb-4">
               <svg
                 className="h-6 w-6 mr-3"
                 xmlns="http://www.w3.org/2000/svg"
@@ -158,7 +168,7 @@ function Home() {
                   d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <h2 className="text-2xl font-semibold">
+              <h2 className="text-2xl font-semibold text-gray-900 ">
                 Story-Centered Stakeholders
               </h2>
             </div>
@@ -167,10 +177,9 @@ function Home() {
               places lived experiences at the center of impact.{" "}
             </p>
             <div className="mt-8 font-semibold flex flex-row">
-              <p className="pt-2">Get to know the actors:</p>
               <Link to="/avatars">
                 <Windmill.Button className="w-65 ml-3 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white">
-                  Click here
+                  Get to Know the Actors
                 </Windmill.Button>
               </Link>
             </div>
@@ -179,7 +188,7 @@ function Home() {
 
         <section id="flower">
           <div className="m-12 mt-24 max-w-7xl px-12">
-            <div className="flex items-center text-gray-900 mb-4">
+            <div className="flex items-center text-pink-dark mb-4">
               <svg
                 className="h-6 w-6 mr-3"
                 xmlns="http://www.w3.org/2000/svg"
@@ -195,7 +204,7 @@ function Home() {
                   d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
                 />
               </svg>
-              <h2 className="text-2xl font-semibold">
+              <h2 className="text-2xl font-semibold text-gray-900 ">
                 Plant the Seed of Change
               </h2>
             </div>
@@ -204,10 +213,9 @@ function Home() {
               of the theory to drive long-term transformation.{" "}
             </p>
             <div className="mt-8 font-semibold flex flex-row">
-              <p className="pt-2">Explore the Flower of Hope:</p>
               <Link to="/flower">
                 <Windmill.Button className="w-65 ml-3 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white">
-                  Click here
+                  Explore the Flower of Hope
                 </Windmill.Button>
               </Link>
             </div>
@@ -220,7 +228,9 @@ function Home() {
         className="bg-gray-50 border-gray-300 border-2 mt-40 pt-12 pb-40"
       >
         <div className="flex flex-col justify-center text-center">
-          This is a footer.
+          The Community TRUST Project is built and maintained by Civic Software
+          Foundation. <a mailto="hi@civicsoftwarefoundation.org">Contact us</a>{" "}
+          for questions.
         </div>
       </section>
 
