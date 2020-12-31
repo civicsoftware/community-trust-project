7f8f9f5189aff67b226bd3ed5cd1de574bb35d44
Stephanie Keske <skeske@gmail.com>
2020-12-30 18:05:33 -0800

    super basic blueprint page:

--- src/Blueprint.jsx
+++ src/Blueprint.jsx
@@ -4,7 +4,46 @@
 import React from "react";
 
 function Blueprint() {
-  return <>This is a blueprint.</>;
+  return (
+    <div className="m-12 mt-20 mb-36 max-w-4xl mx-auto px-12">
+      <div className="flex items-center text-gray-900 mb-4">
+        <svg
+          className="h-6 w-6 mr-3"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+          />
+        </svg>
+        <h2 className="text-2xl font-semibold">Community Library of Context</h2>
+      </div>
+      <p className="block">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+
+      {/* <div className="mt-8 font-semibold flex flex-row">
+        <p className="pt-2">Navigate the Compass of Oppression:</p>
+        <Link to="/compass">
+          <Windmill.Button className="w-65 ml-3 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white">
+            Click here
+          </Windmill.Button>
+        </Link>
+      </div> */}
+    </div>
+  );
 }
 
 export default Blueprint;
