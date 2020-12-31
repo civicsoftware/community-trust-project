424798886ee2e405d01ce77f1d5f7670b96020eb
Stephanie Keske <skeske@gmail.com>
2020-12-31 10:30:19 -0800

    adding basic stuff to blueprint

--- src/Blueprint.jsx
+++ src/Blueprint.jsx
@@ -1,48 +1,76 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-// import { default as Windmill } from "@windmill/react-ui";
-import React from "react";
+import { default as Windmill } from "@windmill/react-ui";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import CivicLogo from "../public/CIVIC_LOGO.svg";
+import InfinityLoop from "../public/INFINITY_LOOP.jpeg";
 
 function Blueprint() {
-  return (
-    <div className="m-12 mt-20 mb-36 max-w-4xl mx-auto px-12">
-      <div className="flex items-center text-gray-900 mb-4">
-        <svg
-          className="h-6 w-6 mr-3"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-          />
-        </svg>
-        <h2 className="text-2xl font-semibold">Community Library of Context</h2>
-      </div>
-      <p className="block">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum
-      </p>
+  const [overlayShown, setOverlayShown] = useState(false);
 
-      {/* <div className="mt-8 font-semibold flex flex-row">
-        <p className="pt-2">Navigate the Compass of Oppression:</p>
-        <Link to="/compass">
-          <Windmill.Button className="w-65 ml-3 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white">
-            Click here
-          </Windmill.Button>
-        </Link>
-      </div> */}
-    </div>
+  function toggleOverlay(e) {
+    setOverlayShown(!overlayShown);
+  }
+
+  return (
+    <>
+      <Windmill.Modal
+        isOpen={overlayShown}
+        onClose={e => toggleOverlay(e)}
+        className="max-w-full mx-40 bg-white border-gray-300 border-2 rounded-lg shadow-xl"
+      >        <img
+          src={InfinityLoop}
+          onClick={e => toggleOverlay(encodeURI)}
+          className="w-full"
+          alt="Infinity Loop"
+        />
+      </Windmill.Modal>
+
+      <div className="m-12 mt-20 mb-36 max-w-4xl mx-auto px-12">
+        <div className="flex items-center text-gray-900 mb-4">
+          <img
+            src={CivicLogo}
+            className="w-7 pt-1 mr-3"
+            alt="Trust Project Logo"
+          />
+          <h2 className="text-2xl font-semibold">
+            Community Library of Context
+          </h2>
+        </div>
+        <p className="block">
+          The Community Library of Context is a resource that reflects an active
+          anti-oppression practice, and a structured process of creating
+          metadata context for datasets that can be curated collaboratively and
+          shared publicly.
+        </p>
+
+        {/* TODO: make this link to the PDF document */}
+        <div className="mt-4 mb-8 flex flex-row">
+          <Link to="/compass">
+            <Windmill.Button className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white">
+              Read the CIVIC Blueprint for Context-Aware Systems
+            </Windmill.Button>
+          </Link>
+        </div>
+        <p className="pt-2">
+          This is a paragraph about the inifinity loop. This is a paragraph
+          about the inifinity loop.This is a paragraph about the inifinity
+          loop.This is a paragraph about the inifinity loop.This is a paragraph
+          about the inifinity loop.
+        </p>
+        <p className="pt-2">
+          Click to view larger:
+        </p>
+
+        <img
+          src={InfinityLoop}
+          onClick={e => toggleOverlay(e, name)}
+          className="mt-10"
+          alt="Infinity Loop"
+        />
+      </div>
+    </>
   );
 }
 
